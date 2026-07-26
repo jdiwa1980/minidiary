63a653f50d32098fa3344e800bc16d630da50974
jdiwa1980 <35224714+jdiwa1980@users.noreply.github.com>
2026-07-27 00:53:57 +0800

    added cors origins

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,13 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://your-vercel-app.vercel.app"
+    ],
+    credentials: true
+}));
 app.use(express.json()); //middleware to prepare request. parses json
 
 app.use("/api/diary", require("./routes/diaryRoutes"));
